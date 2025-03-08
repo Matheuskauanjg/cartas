48f0dc48d8428ace4726fe6923d0c530a8406f42
Matheus kauan pinto <mkauanpinto@gmail.com>
2025-03-08 10:20:24 +0000

    sds

--- cartas-online/src/config/timerConfig.js
+++
@@ -1,7 +0,0 @@
-// src/config/timerConfig.js
-export const TIMER_CONFIG = {
-    cardTime: 30,  // Tempo para enviar a carta (30 segundos)
-    judgeTime: 15, // Tempo para o juiz escolher o vencedor (15 segundos)
-    tieTime: 0,    // Tempo de empate (não é usado diretamente, mas pode ser útil)
-  };
-  
